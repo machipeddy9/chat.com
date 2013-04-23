e506bf02bbb4cfa5bd075063c3940031155f0546
machipeddy9@gmail.comgit config --global user.email machipeddy9@gmail.com <machipeddy9@gmail.com>
2013-04-23 18:47:22 +0530

    updated chat code

---
+++ chat/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Chat::Application.config.secret_token = '73950842bb9169cb4121ef721ea4c85a6063fd21188ae864a5830605b49f897ac3ecd3bf614b72ce82cdf45a5f69e1ca32ab52ebdc0e1fdfb4dc9d7c87e8ac67'
